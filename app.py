975130346bf949e06f483a008f76640088027f7e
Olatunde Ebenezer Obaloluwa <olatundeobaloluwa@gmail.com>
2025-01-16 19:23:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -676,6 +676,6 @@ def serve_react_app(path):
 
 if __name__ == '__main__':
     with app.app_context():
-        db.drop_all()
+
         db.create_all()
     app.run(host='0.0.0.0', port=5000, debug=True)
